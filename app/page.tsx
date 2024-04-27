40810b25f7ff944b94d632f854440190e2f2b3a6
AkbarjonUZB <akbarjonraxmonov1997@gmail.com>
2024-04-27 12:44:46 +0500

    navbarni boshladim

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
-import Image from "next/image";
+
+import Navbar from "./(root)/navbar/navbar";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-       <div>salom bmi</div>
-    </main>
+       <div>
+          <Navbar/>
+          <div className="text-sky-">salom bmi</div>
+       </div>
   );
 }
